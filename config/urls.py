64b57f3744703c86dfc5de4cc0c7e9621267ea2e
chansule92 <57983744+chansule92@users.noreply.github.com>
2025-10-12 06:55:42 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from mainapp.views import health_check
 
 urlpatterns = [
+    path('', health_check), 
     path("mainapp/", include("mainapp.urls")),
     path("admin/", admin.site.urls),
     path("result/", include("mainapp.urls")),
