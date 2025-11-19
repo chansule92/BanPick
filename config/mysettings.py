603a126dba40156628a632a507f832c472e8d64d
chansule92 <57983744+chansule92@users.noreply.github.com>
2025-11-19 16:21:48 +0900

    Update mysettings.py

--- config/mysettings.py
+++ config/mysettings.py
@@ -5,10 +5,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600 
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'banpick_postgresql',   
+        'USER': 'banpick_postgresql_user',  
+        'PASSWORD': 'WL9YPmomzpLbwuvgP8lbs9imR55Zb6q2', 
+        # (Render 내부 네트워크 통신용 주소)
+        'HOST': 'postgresql://banpick_postgresql_user:WL9YPmomzpLbwuvgP8lbs9imR55Zb6q2@dpg-d4emop0gjchc73fka7gg-a/banpick_postgresql', 
+        'PORT': '5432',  
+    }
 }
-
 SECRET_KEY = 'django-insecure-n@5*w)f0u*o#^i47f(6&ec%8clz#*j@inf5kb4kn0ngchwft43'
