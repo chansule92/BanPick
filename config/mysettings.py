b346d43c8982c5e96cfbf6c77919b7a90a91b190
chansule92 <57983744+chansule92@users.noreply.github.com>
2025-11-19 16:10:24 +0900

    Update mysettings.py

--- config/mysettings.py
+++ config/mysettings.py
@@ -1,13 +1,14 @@
+import os
+import dj_database_url
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent 
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'loldb',
-#        'USER': 'root',
-#        'PASSWORD': 'glemfk12',
-#        'HOST': 'localhost',
-#        'PORT': 3306
-#    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600 
+    )
 }
 
 SECRET_KEY = 'django-insecure-n@5*w)f0u*o#^i47f(6&ec%8clz#*j@inf5kb4kn0ngchwft43'
