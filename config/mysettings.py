c504686a8138b3078231a2f74d20daefafe8f857
chansule92 <57983744+chansule92@users.noreply.github.com>
2025-11-19 15:58:57 +0900

    Update mysettings.py

--- config/mysettings.py
+++ config/mysettings.py
@@ -1,5 +1,5 @@
-# 현재 데이터베이스의 값을 입력한다.
-#DATABASES = {
+
+DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.mysql',
 #        'NAME': 'loldb',
@@ -7,8 +7,7 @@
 #        'PASSWORD': 'glemfk12',
 #        'HOST': 'localhost',
 #        'PORT': 3306
-    }
+#    }
 }
 
-# settings.py에 있던 시크릿 키를 아래 ''안에 입력한다.
 SECRET_KEY = 'django-insecure-n@5*w)f0u*o#^i47f(6&ec%8clz#*j@inf5kb4kn0ngchwft43'
